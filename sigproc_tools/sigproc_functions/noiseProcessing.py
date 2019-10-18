deb0cfaa9098efcb706bfe9204bb7b82e03031d7
Usher, Tracy L <usher@slac.stanford.edu>
2019-10-18 08:41:18 -0700

    Adding an example noise mitigation function using a morphological filter approach

--- sigproc_tools/sigproc_functions/noiseProcessing.py
+++ sigproc_tools/sigproc_functions/noiseProcessing.py
@@ -5,6 +5,7 @@
 # Assuming input waveforms have last dimension to be the waveforms
 
 import numpy as np
+import scipy.ndimage as ndimage
 
 def getPedestalsAndRMS(waveforms):
     pedestals = np.mean(waveforms,axis=-1)
@@ -15,7 +16,7 @@ def getPedestalsAndRMS(waveforms):
     rms = np.sqrt(np.mean(np.square(waveLessPed),axis=-1))
     return waveLessPed,pedestals,rms
 
-def getCoherentNoiseCorrection(waveforms):
+def getMedianNoiseCorrection(waveforms):
     median           = np.median(waveforms,axis=0)
     waveLessCoherent = waveforms - median.transpose()
     rms              = np.sqrt(np.mean(np.square(waveLessCoherent),axis=0))
@@ -30,7 +31,7 @@ def removeCoherentNoise(waveforms,grouping,nTicks):
     nChannels = waveforms.shape[0]
     
     for idx in range(0,nChannels,grouping):
-        temp,tempMed,tempRMS = getCoherentNoiseCorrection(waveforms[idx:idx+grouping,:])
+        temp,tempMed,tempRMS = getMedianNoiseCorrection(waveforms[idx:idx+grouping,:])
         if idx == 0:
             waveLessCoherent = temp
             median           = tempMed
@@ -43,5 +44,26 @@ def removeCoherentNoise(waveforms,grouping,nTicks):
     median       = median.transpose().reshape(nChannels//grouping,nTicks)         # Now we make a 2D array of 384 channels by 4096+2 ticks
     intrinsicRMS = intrinsicRMS.transpose().reshape(nChannels//grouping,nTicks)   # Now we make a 2D array of 384 channels by 4096+2 ticks
 
+    return waveLessCoherent,median,intrinsicRMS
+
+def removeCoherentNoiseMorph(waveforms,grouping,nTicks,structuringElement=(3,6)):
+    nChannels = waveforms.shape[0]
+    nGroups   = nChannels // grouping
+    
+    # Define placeholders for the output arrays
+    waveLessCoherent = np.zeros(waveforms.shape)
+    median           = np.zeros((nGroups,nTicks))
+    intrinsicRMS     = np.zeros((nGroups,nTicks))
+    
+    for idx in range(0,nChannels,grouping):
+        erosion   = ndimage.grey_erosion(waveforms[idx:idx+grouping,:],size=structuringElement)
+        dilation  = ndimage.grey_dilation(waveforms[idx:idx+grouping,:],size=structuringElement)
+        average   = (dilation+erosion) / 2.
+        thisGroup = idx // grouping
+
+        median[thisGroup,:]                  = np.median(average,axis=0)
+        waveLessCoherent[idx:idx+grouping,:] = waveforms[idx:idx+grouping,:] - median[thisGroup,:]
+        intrinsicRMS[thisGroup,:]            = np.sqrt(np.mean(np.square(waveLessCoherent[idx:idx+grouping,:]),axis=0))
+        
     return waveLessCoherent,median,intrinsicRMS
 
